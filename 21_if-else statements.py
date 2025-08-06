7c0e0971c13a62ba64d484c8cbd72fa55f4445cf
Waqas-Afridi-07 <afridiwaqas571@gmail.com>
2025-08-06 09:07:02 +0500

    Challenge question

---
+++ 21_if-else statements.py
@@ -0,0 +1,13 @@
+is_hot = input("Is it hot?: ")
+is_cold = input("Is it cold?: ")
+
+if is_hot == "yes":
+    print("It's a hot day")
+    print("Drink plenty of water")
+
+elif is_cold == "yes":
+    print("It's a cold day")
+    print("Wear warm clothes")
+
+else:
+    print("It's a lovely day")
