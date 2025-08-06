671c67b14c3167867b0d3c4462966dc6eeef7bd0
Waqas-Afridi-07 <afridiwaqas571@gmail.com>
2025-08-06 05:37:25 +0500

    Different cases using quotes

---
+++ 09_String cases in quotes.py
@@ -0,0 +1,20 @@
+# Cases in using single and double quotes.
+
+# course = 'Python's Course for beginnners' 
+
+course_1 ="Python's Course for Beginnners" # for using apostrophe
+print(course_1) 
+
+course_2 ='Python for "Beginnners"' # for wrapping a word in double quotes
+print(course_2)
+
+# Triple quotes are used when string is long and has a lot to define
+
+course_3 = ''' 
+Hi Zia,  # 
+This is our first email to you
+
+Thanks,
+The support team
+'''
+print(course_3)
