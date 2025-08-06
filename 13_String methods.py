6b9de1bffd781230fec192ef98fbf6ef832c678e
Waqas-Afridi-07 <afridiwaqas571@gmail.com>
2025-08-06 07:42:14 +0500

    Different string methods

---
+++ 13_String methods.py
@@ -0,0 +1,16 @@
+course = "Python for beginners"
+print(len(course))
+
+print(course.lower())
+
+print(course.upper()) # functions does not changes the original string
+print(course)
+
+print(course.find("o"))
+print(course.find("beginners"))
+
+print(course.replace("for", "For"))
+
+print("Python" in course)
+
+print(course.title())
