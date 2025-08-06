dc8525cb8e5ca6f0df7f305afcaeea50bab631c6
Waqas-Afridi-07 <afridiwaqas571@gmail.com>
2025-08-06 06:04:47 +0500

    Cases of indexing

---
+++ 10_Types of string indexing.py
@@ -0,0 +1,24 @@
+# String is a sequence of characters enclosed in quotes.
+
+course = "Python for Beginners"
+
+character = course[0]
+print(character)
+
+character = course[-1] # Negative indexing
+print(character)
+
+character = course[0:6] # Accessing more than one characters (lats character is excluded)
+print(character)
+
+character = course[0:] # This prints all characters assuming 0:length
+print(character)
+
+character = course[:3] # This prints from 0 to 3 assuming 0:...
+print(character) 
+
+character = course[:] # This prints all characters assuming 0:....
+print(character)
+
+character = course[-9:-1] # Accessing more than one characters using negative indexing
+print(character)
